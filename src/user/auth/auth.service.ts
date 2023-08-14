ab9b2071ef6995be7157e925df80ebbb6cf9a418
andreenakashima <andreenakashima@gmail.com>
2023-08-14 16:14:57 -0300

    Added create user

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { UserType } from '@prisma/client';
 
 interface SignUpParams {
   email: string;
@@ -11,13 +13,27 @@ interface SignUpParams {
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  signUp({ email }: SignUpParams) {
-    const userExists = this.prismaService.user.findUnique({
+  async signUp({ email, password, name, phone }: SignUpParams) {
+    const userExists = await this.prismaService.user.findUnique({
       where: { email },
     });
 
     if (userExists) {
       throw new ConflictException();
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    return user;
   }
 }
