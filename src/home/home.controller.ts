9146e6fcf3e263a4563b33b828a9802a5d0d4dc4
andreenakashima <andreenakashima@gmail.com>
2023-08-16 10:17:58 -0300

    Added homes list

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { HomeService } from './home.service';
+import { HomeResponseDto } from './dto/home.dto';
 
 @Controller('home')
 export class HomeController {
+  constructor(private readonly homeService: HomeService) {}
+
   @Get()
-  getHomes() {
-    return [];
+  getHomes(): Promise<HomeResponseDto[]> {
+    return this.homeService.getHomes();
   }
 
   @Get(':id')
