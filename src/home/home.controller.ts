3af599f4c405d5b374014fc1db06b5c3cc1e45b4
andreenakashima <andreenakashima@gmail.com>
2023-08-22 15:27:45 -0300

    Added user interceptor

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -12,6 +12,7 @@ import {
 import { HomeService } from './home.service';
 import { CreateHomeDto, HomeResponseDto, UpdateHomeDto } from './dto/home.dto';
 import { PropertyType } from '@prisma/client';
+import { User } from 'src/user/decorators/user.decorators';
 
 @Controller('home')
 export class HomeController {
@@ -46,7 +47,7 @@ export class HomeController {
   }
 
   @Post()
-  createHome(@Body() body: CreateHomeDto) {
+  createHome(@Body() body: CreateHomeDto, @User() user) {
     return this.homeService.createHome(body);
   }
 
