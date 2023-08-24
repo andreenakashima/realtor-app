2f1f40be27304a1e290b39ca0b0244de018ded74
andreenakashima <andreenakashima@gmail.com>
2023-08-24 11:06:49 -0300

    Added getHomes test

--- src/home/home.service.spec.ts
+++ src/home/home.service.spec.ts
@@ -1,18 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HomeService } from './home.service';
+import { HomeService, homeSelect } from './home.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { PropertyType } from '@prisma/client';
+
+const mockGetHomes = [
+  {
+    id: 1,
+    address: 'Rua teste',
+    city: 'Curitiba',
+    price: 1000000,
+    propertyType: PropertyType.RESIDENTIAL,
+    img: 'img1',
+    numberOfBedrooms: 4,
+    numberOfBathrooms: 4,
+    images: [
+      {
+        url: 'src1',
+      },
+    ],
+  },
+];
 
 describe('HomeService', () => {
   let service: HomeService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HomeService],
+      providers: [
+        HomeService,
+        {
+          provide: PrismaService,
+          useValue: {
+            home: {
+              findMany: jest.fn().mockReturnValue(mockGetHomes),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<HomeService>(HomeService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getHomes', () => {
+    const filters = {
+      city: 'Curitiba',
+      price: {
+        gte: 1000000,
+        lte: 1500000,
+      },
+      propertyType: PropertyType.RESIDENTIAL,
+    };
+
+    it('should call prism home.findMany with correct params', async () => {
+      const mockPrismaFindManyHomes = jest.fn().mockReturnValue(mockGetHomes);
+
+      jest
+        .spyOn(prismaService.home, 'findMany')
+        .mockImplementation(mockPrismaFindManyHomes);
+
+      await service.getHomes(filters);
+
+      expect(mockPrismaFindManyHomes).toBeCalledWith({
+        select: {
+          ...homeSelect,
+          images: {
+            select: {
+              url: true,
+            },
+            take: 1,
+          },
+        },
+        where: filters,
+      });
+    });
   });
 });
